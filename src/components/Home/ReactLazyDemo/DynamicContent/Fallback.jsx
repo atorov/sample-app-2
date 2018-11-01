ae17448562e62013329baa3169276f3add32232d
Veselin <veselin.lk.petrov@gmail.com>
2018-11-01 21:11:39 +0200

    refactored ReactLazyDemo component

---
+++ src/components/Home/ReactLazyDemo/DynamicContent/Fallback.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import CircularProgress from '@material-ui/core/CircularProgress'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
+
+export default () => (
+    <ExpansionPanelDetails>
+        <CircularProgress />
+    </ExpansionPanelDetails>
+)
--- src/components/Home/ReactLazyDemo/DynamicContent/index.jsx
+++ src/components/Home/ReactLazyDemo/DynamicContent/index.jsx
@@ -1,22 +1,19 @@
-import React, { Suspense } from 'react'
-
-import CircularProgress from '@material-ui/core/CircularProgress'
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
+import React, {
+    lazy,
+    Suspense,
+} from 'react'
 
 import delay from '../../../../lib/delay'
 
 import ErrorBoundary from '../../../ErrorBoundary'
 
-const Content = React.lazy(() => delay(1000).then(() => import('./Content'))) // eslint-disable-line
+import Fallback from './Fallback'
+
+const Content = lazy(() => delay(1000).then(() => import('./Content'))) // eslint-disable-line
 
 const DynamicContent = (props) => (
     <ErrorBoundary>
-        <Suspense fallback={(
-            <ExpansionPanelDetails>
-                <CircularProgress />
-            </ExpansionPanelDetails>
-        )}
-        >
+        <Suspense fallback={<Fallback />}>
             <Content {...props} />
         </Suspense>
     </ErrorBoundary>
